54bf974dcfa75afda5dea2250c8a91301134cd41
amine9005 <amamselmitrader@gmail.com>
2023-06-03 19:02:16 +0100

    workflow

--- src/app/user-avatar/user-avatar.component.ts
+++ src/app/user-avatar/user-avatar.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-avatar',
   templateUrl: './user-avatar.component.html',
-  styleUrls: ['./user-avatar.component.scss']
+  styleUrls: ['./user-avatar.component.scss'],
 })
 export class UserAvatarComponent {
-  hide_menu: boolean = true;
+  hide_menu = true;
 
   toggleMenu() {
-    this.hide_menu =!this.hide_menu;
+    this.hide_menu = !this.hide_menu;
   }
-
 }
