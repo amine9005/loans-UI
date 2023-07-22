af9cd76cc6f98b3a79c4d71c496eaed120fa64b3
amine9005 <amamselmitrader@gmail.com>
2023-07-22 23:15:35 +0100

    List Product

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { response } from 'src/app/redux/customers/customers.types';
 
+interface Product {
+  name: string;
+  thumbnail: string;
+  pictures: string;
+  slag: string;
+  price: string;
+  quantity: string;
+}
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
-
+  constructor(
+    private store: Store<{
+      customers: response;
+    }>
+  ) {}
+  listOfData: Product[] = [];
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule } from '@angular/forms';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { ProductsRoutingModule } from './products-routing.module';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ProductsRoutingModule
-  ]
+    NzTableModule,
+    NzDividerModule,
+    NzGridModule,
+    NzInputModule,
+    NzSelectModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzButtonModule,
+    FormsModule,
+    NzIconModule,
+    ProductsRoutingModule,
+  ],
 })
-export class ProductsModule { }
+export class ProductsModule {}
