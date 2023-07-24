ee006b8dd408b10f499bb75c40b6dba70578eebc
amine9005 <amamselmitrader@gmail.com>
2023-07-24 22:13:00 +0100

    added search ui

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -5,9 +5,10 @@ import { EditComponent } from './edit/edit.component';
 import { DeleteComponent } from './delete/delete.component';
 import { AddComponent } from './add/add.component';
 import { SearchComponent } from './search/search.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: ListComponent },
+  { path: '', canActivate: [AuthGuard], component: ListComponent },
   { path: 'edit/:id', component: EditComponent },
   { path: 'delete/:id', component: DeleteComponent },
   { path: 'add', component: AddComponent },
--- src/app/products/search/search.component.ts
+++ src/app/products/search/search.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
+  selectBy = 'By Name';
+  selections = [
+    'By Name',
+    'By Id',
+    'Price Greater Than',
+    'Price Lower Than',
+    'Price Equal To',
+    'Quantity Greater Than',
+    'Quantity Lower Than',
+    'Quantity Equal To',
+  ];
+  searchTerm = '';
 
+  searchBy(value: string): void {
+    this.selectBy = value;
+  }
+
+  search() {
+    console.log('Searching...');
+  }
 }
