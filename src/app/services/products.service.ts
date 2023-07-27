b6f96a76a10f91c4507f5a70c3fb6f6f2de88f4f
amine9005 <amamselmitrader@gmail.com>
2023-07-27 22:12:48 +0100

    added products service

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import httpCommon from '../utils/http-common';
+import { Store } from '@ngrx/store';
+import { userSate } from '../redux/auth/user.types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private store: Store<{ user: userSate }>) {}
+
+  getProducts() {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.get('/products', config);
+  }
+}
