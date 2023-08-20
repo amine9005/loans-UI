16c8693f57c23b04e861c13858063d561147f220
amine9005 <amamselmitrader@gmail.com>
2023-08-20 22:43:35 +0100

    added order types

---
+++ src/app/redux/orders/orders.types.ts
@@ -0,0 +1,25 @@
+export interface order {
+  _id: string;
+  orderItems: Array<string>;
+  shippingAddress: string;
+  paymentMethod: string;
+  itemsPrice: Array<number>;
+  shippingPrice: number;
+  totalPrice: number;
+}
+
+export interface productsArray extends Array<order> {
+  _id: string;
+  orderItems: Array<string>;
+  shippingAddress: string;
+  paymentMethod: string;
+  itemsPrice: Array<number>;
+  shippingPrice: number;
+  totalPrice: number;
+}
+
+export interface response {
+  isLoading: boolean;
+  error: boolean;
+  data: Array<productsArray> | any;
+}
