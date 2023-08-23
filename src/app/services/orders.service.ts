c13a8d872d6222ad9a80ea4db026446f3553c715
amine9005 <amamselmitrader@gmail.com>
2023-08-23 22:15:18 +0100

    Orders add,update,delete and get

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import httpCommon from '../utils/http-common';
 import { Store } from '@ngrx/store';
 import { userSate } from '../redux/auth/user.types';
-
+import { order } from '../redux/orders/orders.types';
 @Injectable({
   providedIn: 'root',
 })
 export class OrdersService {
   constructor(private store: Store<{ user: userSate }>) {}
 
-  getOrderss() {
+  getOrders() {
     let token = '';
     this.store.select('user').subscribe((data) => {
       token = data.token;
@@ -19,5 +19,53 @@ export class OrdersService {
       headers: { Authorization: `Bearer ${token}` },
     };
     return httpCommon.get('/orders', config);
+  }
+
+  addOrder(data: order) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.post('/orders', data, config);
+  }
+
+  updateOrder(id: string, data: order) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.put('/orders/update/' + id, data, config);
+  }
+
+  deleteOrder(id: string) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.delete('/orders/delete/' + id, config);
+  }
+
+  getOrderById(id: string) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.get('/orders/orders/' + id, config);
   }
 }
