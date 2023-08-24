63e195222f97465852a99c9f3c6726d9fd13f465
amine9005 <amamselmitrader@gmail.com>
2023-08-24 22:38:19 +0100

    added add , delete, update and get product by id

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import httpCommon from '../utils/http-common';
 import { Store } from '@ngrx/store';
 import { userSate } from '../redux/auth/user.types';
+import { product } from '../redux/products/products.types';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,53 @@ export class ProductsService {
       headers: { Authorization: `Bearer ${token}` },
     };
     return httpCommon.get('/products', config);
+  }
+
+  addProduct(data: product) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.post('/products', data, config);
+  }
+
+  getProductById(id: string) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.get('/' + id, config);
+  }
+
+  deleteProduct(id: string) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.delete('/' + id, config);
+  }
+
+  updateProduct(id: string, data: product) {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.put('/' + id, data, config);
   }
 }
