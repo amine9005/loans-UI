f2e35a20b0ed2769528478bef95c0f8ddc4cdec7
amine9005 <amamselmitrader@gmail.com>
2023-09-20 21:10:24 +0100

    update user UI

--- src/app/customers/update/update.component.ts
+++ src/app/customers/update/update.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  UntypedFormControl,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.scss']
+  styleUrls: ['./update.component.scss'],
 })
 export class UpdateComponent {
+  maxDate = new Date();
+  validateForm!: FormGroup;
 
+  submitForm(): void {
+    console.log('Updating');
+  }
 }
