b0135250c918c37668c77753ffba0095ab6c5786
amine9005 <amamselmitrader@gmail.com>
2023-09-30 22:57:22 +0100

    fixes

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { EditComponent } from './edit/edit.component';
-import { DeleteComponent } from './delete/delete.component';
 import { AddComponent } from './add/add.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'edit/:id', component: EditComponent },
-  { path: 'delete/:id', component: DeleteComponent },
   { path: 'add', component: AddComponent },
   { path: 'search', component: SearchComponent },
 ];
