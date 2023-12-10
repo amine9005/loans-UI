f0a7cceadb8c116f683239a47e19634d83c97f26
amine9005 <amamselmitrader@gmail.com>
2023-12-10 20:56:20 +0100

    dashboard services

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import httpCommon from '../utils/http-common';
+import { Store } from '@ngrx/store';
+import { userSate } from '../redux/auth/user.types';
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  constructor(private store: Store<{ user: userSate }>) {}
+
+  getTotalSales() {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.get('/dashboard/totalSales', config);
+  }
+
+  getTotalProducts() {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.get('/dashboard/totalProducts', config);
+  }
+
+  getInventory() {
+    let token = '';
+    this.store.select('user').subscribe((data) => {
+      token = data.token;
+    });
+    const config = {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return httpCommon.get('/dashboard/inventory', config);
+  }
+}
