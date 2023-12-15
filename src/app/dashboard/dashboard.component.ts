4c6a6aff4105b07d1ff52b926dd991a4c7d99fc6
amine9005 <amamselmitrader@gmail.com>
2023-12-15 23:22:13 +0100

    getInventory Service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   totalSales = 31700;
   totalOrders = 124;
   totalProducts = 69;
@@ -14,4 +15,18 @@ export class DashboardComponent {
     labels: ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'],
     datasets: [{ data: [31, 65, 69, 71, 59, 66, 68], label: 'Sales' }],
   };
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.dashboardService
+      .getInventory()
+      .then((resp) => {
+        this.totalProducts = resp.data['products'];
+      })
+      .catch((err) => {
+        this.totalProducts = -1;
+        console.log('error: ', err.message);
+      });
+  }
 }
